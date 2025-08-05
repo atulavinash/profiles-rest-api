d8866034722d757637e10400f797af23ad4891e0
Atul <avinash.atul@gmail.com>
2025-08-05 10:17:00 +0530

    added viewset to profiles api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -53,3 +54,58 @@ class HelloApiView(APIView):
         """ Delete an object """
 
         return Response({'method' : 'delete'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+
+    """Test API ViewSet"""
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self,request):
+        """ return a Hello message"""
+
+        a_viewset = [
+            "uses actions (list,create,retreive,update,partial_update,delete)",
+            "Automatically maps to URLs using routers",
+            "Provides more functionality with less code",
+
+        ]
+        return Response({"message": 'Helloview', "a_viewset" : a_viewset})
+
+    def create(self,request):
+        """Create a new Hello message"""
+
+        serializer = self.serializer_class(data = request.data)
+
+        if serializer.is_valid():
+
+            name = serializer.validated_data.get('name')
+            message = f'Hello{name}'
+            return Response({'message': message})
+
+        else:
+
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+
+            )
+
+    def retrieve(self,request, pk=None):
+        """Handle getting an object by its id"""
+        return Response({'http_method' : 'GET'})
+
+    def update(self,request,pk = None):
+        """handle update an object"""
+
+        return Response({'http_method' : 'PUT'})
+
+    def partial_update(self,request, pk = None):
+        """handle Partial update of an object"""
+
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self,request,pk = None):
+        """Handle removing an object"""
+        return Response({'http_method': 'DELETE'})
